7b4b72371210e773201fa26df055d1ecf57df237
CMonjo <camille@cmonjo.com>
2024-06-10 19:15:30 +0200

    feat(insights): added package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css'
 import { Kobe11 } from '@/app/ui/fonts'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${Kobe11.className}`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
