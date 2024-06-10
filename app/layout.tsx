b58048bf556f3f62c35ff5f7a9c76c6e880d96d0
CMonjo <camille@cmonjo.com>
2024-06-10 19:04:08 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import '@/app/ui/global.css';
-import { Kobe11 } from '@/app/ui/fonts';
+import '@/app/ui/global.css'
+import { Kobe11 } from '@/app/ui/fonts'
+import { Analytics } from '@vercel/analytics/react'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${Kobe11.className}`}>{children}</body>
+      <body className={`${Kobe11.className}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  );
+  )
 }
