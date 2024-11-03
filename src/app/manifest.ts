34d71551a5061160713acbfdb26c0ada872ad38f
CMonjo <camille@cmonjo.com>
2024-11-03 15:59:11 +0100

    fix(manifest): build error

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,10 +2,10 @@ import { MetadataRoute } from 'next';
 import { getTranslations } from 'next-intl/server';
 
 export default async function manifest(): Promise<MetadataRoute.Manifest> {
-  const t = await getTranslations({ namespace: 'Manifest' });
+  //   const t = await getTranslations({ namespace: 'Manifest' });
 
   return {
-    name: t('name'),
+    name: /*t('name')*/ 'test',
     start_url: '/',
     theme_color: '#101E33', //TODO Camille
   };
