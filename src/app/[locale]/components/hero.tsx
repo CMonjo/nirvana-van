b8b588bd6173d1414843cafc2f5d55458af82b5a
CMonjo <camille@cmonjo.com>
2025-05-11 17:04:03 +0200

    removed xl

--- src/app/[locale]/components/hero.tsx
+++ src/app/[locale]/components/hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
         alt='hero'
       />
 
-      <div className='absolute top-12 flex w-full flex-col items-center justify-center md:top-16 lg:top-[100px] xl:top-[120px]'>
+      <div className='absolute top-12 flex w-full flex-col items-center justify-center md:top-16 lg:top-[100px]'>
         <HeroTitle>{t('hero.title')}</HeroTitle>
         <Typography
           variant='h3'
